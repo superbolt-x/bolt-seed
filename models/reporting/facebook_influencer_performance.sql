2be4c502ec81497699415745b4d8b50395cc68f6
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-02-27 14:01:28 -0500

    Update facebook_influencer_performance.sql
    added 'Others' theme

--- models/reporting/facebook_influencer_performance.sql
+++ models/reporting/facebook_influencer_performance.sql
@@ -136,6 +136,23 @@ science as
         FROM {{ source('facebook_raw','ads_insights_age_gender_actions') }} GROUP BY 1,2) USING (date,ad_id)
     WHERE ad_name ~* 'INF'
     AND ad_name ~* '_SC_'
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11),
+
+others as
+    (SELECT date, ad_id, ad_name, adset_id, adset_name, campaign_id, campaign_name, age, gender, 'Others' as theme,
+        CASE WHEN ad_name ~* 'Shred' OR ad_name ~* 'Bobby' THEN 'Male'
+            WHEN ad_name ~* 'Shred' AND ad_name !~* 'Bobby' THEN 'Female'
+        END as influencer_gender,
+        COALESCE(SUM(spend),0) as spend,
+        COALESCE(SUM(impressions),0) as impressions,
+        COALESCE(SUM(inline_link_clicks),0) as link_clicks,
+        COALESCE(SUM(purchases),0) as purchases
+    FROM {{ source('facebook_raw','ads_insights_age_gender') }}
+    LEFT JOIN 
+        (SELECT date, ad_id, COALESCE(SUM(CASE WHEN action_type = 'purchase' THEN value END),0) as purchases 
+        FROM {{ source('facebook_raw','ads_insights_age_gender_actions') }} GROUP BY 1,2) USING (date,ad_id)
+    WHERE ad_name ~* 'INF'
+    AND (ad_name !~* '_GH_' AND ad_name !~* '_IH_' AND ad_name !~* '_PF_' AND ad_name !~* '_TBH_' AND ad_name !~* '_PB_' AND ad_name !~* '_SC_')
     GROUP BY 1,2,3,4,5,6,7,8,9,10,11)
 
 SELECT * FROM gut_health 
@@ -146,3 +163,4 @@ SELECT * FROM gut_health
     LEFT JOIN total_body_health_no_shred USING(date, ad_id, ad_name, adset_id, adset_name, campaign_id, campaign_name, age, gender, theme, influencer_gender)
     LEFT JOIN product_benefits USING(date, ad_id, ad_name, adset_id, adset_name, campaign_id, campaign_name, age, gender, theme, influencer_gender)
     LEFT JOIN science USING(date, ad_id, ad_name, adset_id, adset_name, campaign_id, campaign_name, age, gender, theme, influencer_gender)
+    LEFT JOIN others USING(date, ad_id, ad_name, adset_id, adset_name, campaign_id, campaign_name, age, gender, theme, influencer_gender)
