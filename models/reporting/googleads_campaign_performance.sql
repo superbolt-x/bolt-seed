b05573b867966bec72a0b7c0531e4e4ec9c7ee4b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-09-04 16:43:01 +0200

    Update googleads_campaign_performance.sql

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -8,6 +8,19 @@ campaign_name,
 campaign_id,
 campaign_status,
 campaign_type_default,
+case 
+    when campaign_name ~* 'DS01' then 'DS01'
+    when campaign_name ~* 'VS01' then 'VS01'
+    when campaign_name ~* 'PDS08' then 'PDS08'
+    else 'Other'
+end as product,
+case 
+    when campaign_name ~* 'Shopping' then 'Shopping'
+    when (campaign_name ~* 'Performance Max' or campaign_name ~* 'PMax') then 'PMax'
+    when campaign_name ~* 'NB' then 'Non Brand'
+    when campaign_name ~* 'Brand' then 'Branded'
+    else 'Other'
+end as campaign_type_custom,
 date,
 date_granularity,
 spend,
