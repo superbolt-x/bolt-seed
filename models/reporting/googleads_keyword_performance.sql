c8f364b81b025171332643f981be2af419f63271
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-08 17:34:20 -0600

    Update googleads_keyword_performance.sql

--- models/reporting/googleads_keyword_performance.sql
+++ models/reporting/googleads_keyword_performance.sql
@@ -3,13 +3,25 @@
 )}}
 
 {% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
-  
+
 WITH initial_data as 
-    (SELECT *, {{ get_date_parts('date') }} FROM {{ source('googleads_raw', 'keyword_performance_report') }})
+    (SELECT date::date as date, keyword_text as keyword, keyword_match_type as match_type, ad_group_name, ad_group_id, campaign_name, campaign_id,
+        COALESCE(SUM(cost_micros/1000000),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions
+        COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue
+    FROM {{ source('googleads_raw', 'keyword_performance_report') }}
+    LEFT JOIN 
+        (SELECT date,keyword_text,keyword_match_type,ad_group_name,ad_group_id,campaign_name,campaign_id,
+        CASE WHEN conversion_action_name = 'Purchase (Adwords Pixel)' THEN conversions END as purchases, 
+        CASE WHEN conversion_action_name = 'Purchase (Adwords Pixel)' THEN conversions_value END as revenue
+        FROM {{ source('googleads_raw', 'keyword_convtype_performance_report') }}) USING (date,keyword_text,keyword_match_type,ad_group_name,ad_group_id,campaign_name,campaign_id)
+    GROUP BY 1,2,3,4,5,6,7)
+    
+cleaned_data as 
+    (SELECT *, {{ get_date_parts('date') }} FROM initial_data)
     
     {%- for date_granularity in date_granularity_list %}    
     SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
-    keyword_text as keyword, keyword_match_type as match_type, ad_group_name, campaign_name, campaign_id,
+    keyword, match_type, ad_group_name, ad_group_id, campaign_name, campaign_id,
     case 
         when campaign_name ~* 'INTL' then 'INTL'
         else 'US'
@@ -29,8 +41,8 @@ WITH initial_data as
         when campaign_name ~* 'Brand' and campaign_name !~* 'amazon' then 'Branded'
         else 'Other'
     end as campaign_type_custom,
-    COALESCE(SUM(cost_micros/1000000),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(conversions),0) as purchases, 
-    COALESCE(SUM(conversions_value),0) as revenue
+    COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(purchases),0) as purchases, 
+    COALESCE(SUM(revenue),0) as revenue
     FROM initial_data
     GROUP BY 1,2,3,4,5,6,7,8,9,10
     {% if not loop.last %}UNION ALL
