6f12421d9e8cb383b72492e6213ca636d4968bc6
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-08 14:48:03 -0600

    Update facebook_ad_performance.sql
    leads & product

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -9,6 +9,13 @@ campaign_effective_status,
 CASE WHEN campaign_name ~* 'Prospect' OR campaign_name ~* 'Interest' THEN 'Campaign Type: Prospecting'
     WHEN campaign_name ~* 'Retarget' THEN 'Campaign Type: Retargeting'
 END as campaign_type_default,
+case 
+    when campaign_name ~* 'DS01' then 'DS01'
+    when campaign_name ~* 'VS01' then 'VS01'
+    when campaign_name ~* 'PDS08' then 'PDS08'
+    when campaign_name ~* 'HCP' then 'HCP'
+    else 'Other'
+end as product,  
 adset_name,
 adset_id,
 adset_effective_status,
@@ -28,5 +35,7 @@ link_clicks,
 add_to_cart,
 purchases,
 revenue,
-"offsite_conversion.custom.975370734291959" as "VS-01 WK"
+"offsite_conversion.custom.975370734291959" as "VS-01 WK",
+--reach,
+lead as leads
 FROM {{ ref('facebook_performance_by_ad') }}
