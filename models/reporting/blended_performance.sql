bf548c38486b7d121d0ef275659ffb944800e942
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-08 17:38:02 -0600

    Create blended_performance.sql

---
+++ models/reporting/blended_performance.sql
@@ -0,0 +1,83 @@
+{{ config (
+    alias = target.database + '_blended_performance'
+)}}
+
+{% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
+  
+WITH last_updated_data as
+    (SELECT *, MAX(_fivetran_synced) OVER (PARTITION BY activation_date,channel,utm_campaign,utm_content,utm_term) as last_updated_date
+    FROM {{ source('s3_raw','lasttouch_performance') }} 
+    WHERE _file ~* 'grouped')
+    
+    initial_s3_data as 
+    (SELECT *, {{ get_date_parts('activation_date') }} FROM last_updated_data where _fivetran_synced = last_updated_date)
+  
+    s3_data as
+    ({%- for date_granularity in date_granularity_list %}    
+        SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
+            utm_campaign, utm_content, utm_term,
+            CASE WHEN channel ~* 'meta' THEN 'Meta' WHEN channel ~* 'google' THEN 'Google Ads' END as channel,
+            CASE WHEN utm_campaign ~* 'INTL' THEN 'INTL' ELSE 'US' END as market,
+            CASE WHEN utm_campaign ~* 'DS01' THEN 'DS01'
+                WHEN utm_campaign ~* 'VS01' THEN 'VS01'
+                WHEN utm_campaign ~* 'PDS08' THEN 'PDS08'
+                ELSE 'Other'
+            END as product,    
+            CASE 
+                WHEN utm_campaign ~* 'YT' AND utm_campaign !~* 'amazon' THEN 'Youtube'
+                WHEN utm_campaign ~* 'Shopping' AND utm_campaign ~* 'Brand' AND utm_campaign !~* 'amazon' THEN 'Shopping - Brand'
+                WHEN utm_campaign ~* 'Shopping' AND utm_campaign !~* 'Brand' AND utm_campaign !~* 'amazon' THEN 'Shopping - Non Brand'
+                WHEN (utm_campaign ~* 'Performance Max' OR utm_campaign ~* 'PMax') AND utm_campaign !~* 'amazon' THEN 'PMax'
+                WHEN utm_campaign ~* 'NB' AND utm_campaign !~* 'amazon' THEN 'Non Brand'
+                WHEN utm_campaign ~* 'Brand' AND utm_campaign !~* 'amazon' THEN 'Branded'
+                ELSE 'Other'
+            END as campaign_type,
+            COALESCE(SUM(fta_subs),0) as ft_orders, COALESCE(SUM(lta_subs),0) as lt_orders
+        FROM initial_s3_data
+        {% if not loop.last %}UNION ALL
+        {% endif %}
+    {% endfor %})
+  
+    final_data as
+    (SELECT channel, date::date, date_granularity, market, product, utm_campaign, campaign_type, utm_content, utm_term,
+        COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, /*COALESCE(SUM(reach),0) as reach,*/
+        COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(leads),0) as leads, COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM("VS-01 WK"),0) as "VS-01 WK",
+        COALESCE(SUM(revenue),0) as revenue, COALESCE(SUM(ft_orders),0) as ft_orders, COALESCE(SUM(lt_orders),0) as lt_orders
+    FROM
+        (SELECT 'Meta' as channel, date, date_granularity, null as market, product, campaign_name as utm_campaign, campaign_type_default as campaign_type,
+            adset_name as utm_content, ad_name as utm_term,
+            spend, impressions, link_clicks as clicks, /*reach,*/ add_to_cart, leads, purchases, "VS-01 WK", 0 as revenue, 0 as ft_orders, 0 as lt_orders
+        FROM {{ source('reporting','facebook_ad_performance') }}
+        UNION ALL
+        SELECT 'Google Ads' as channel, date, date_granularity, country as market, product, campaign_name as utm_campaign, campaign_type_custom as campaign_type, 
+            null as utm_content, keyword as utm_term,
+            spend, impressions, clicks, /*0 as reach,*/ 0 as add_to_cart, 0 as leads, purchases, 0 as "VS-01 WK", revenue, 0 as ft_orders, 0 as lt_orders
+        FROM {{ source('reporting','googleads_keyword_performance') }}
+        UNION ALL
+        SELECT channel, date, date_granularity, market, product, utm_campaign, campaign_type, utm_content, utm_term,
+            0 as spend, 0 as impressions, 0 as clicks, /*0 as reach,*/ 0 as add_to_cart, 0 as leads, 0 as purchases, 0 as "VS-01 WK", 0 as revenue, ft_orders, lt_orders
+        FROM s3_data
+        )
+    GROUP BY channel, date, date_granularity, market, product, utm_campaign, campaign_type, utm_content, utm_term)
+    
+SELECT channel, 
+  date, 
+  date_granularity, 
+  market, 
+  product, 
+  utm_campaign, 
+  campaign_type, 
+  utm_content, 
+  utm_term,
+  spend,
+  impressions,
+  clicks,
+  /*reach,*/
+  add_to_cart,
+  leads,
+  purchases,
+  "VS-01 WK",
+  revenue,
+  ft_orders,
+  lt_orders
+FROM paid_data
