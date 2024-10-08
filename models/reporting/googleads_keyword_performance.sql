488f2e4ef80619dbda184807334341d5e18dc44f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-08 17:16:34 -0600

    Update googleads_keyword_performance.sql
    date_parts

--- models/reporting/googleads_keyword_performance.sql
+++ models/reporting/googleads_keyword_performance.sql
@@ -4,7 +4,10 @@
 
 {% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
   
-  {%- for date_granularity in date_granularity_list %}    
+WITH initial_data as 
+    (SELECT *, {{ get_date_parts('date') }} FROM {{ source('googleads_raw', 'keyword_performance_report') }})
+    
+    {%- for date_granularity in date_granularity_list %}    
     SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
     keyword_text as keyword, keyword_match_type as match_type, ad_group_name, campaign_name, campaign_id,
     case 
@@ -28,7 +31,7 @@
     end as campaign_type_custom,
     COALESCE(SUM(cost_micros/1000000),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(conversions),0) as purchases, 
     COALESCE(SUM(conversions_value),0) as revenue
-    FROM {{ source('googleads_raw', 'keyword_performance_report') }}
+    FROM initial_data
     GROUP BY 1,2,3,4,5,6,7,8,9,10
     {% if not loop.last %}UNION ALL
     {% endif %}
