388121fbaba327de595f1ce951f27594d9213559
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-11 14:25:00 -0600

    Update blended_performance.sql
    google_campaign

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -15,7 +15,8 @@ WITH last_updated_data as
     s3_data as
     ({%- for date_granularity in date_granularity_list %}    
         SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
-            utm_campaign, utm_content, utm_term,
+            utm_campaign::varchar, campaign_name::varchar as google_campaign,
+            utm_content, utm_term,
             CASE WHEN channel ~* 'meta' THEN 'Meta' WHEN channel ~* 'google' THEN 'Google Ads' END as channel,
             CASE WHEN utm_campaign ~* 'INTL' THEN 'INTL' ELSE 'US' END as market,
             CASE WHEN utm_campaign ~* 'DS01' THEN 'DS01'
@@ -34,44 +35,52 @@ WITH last_updated_data as
             END as campaign_type,
             COALESCE(SUM(fta_subs),0) as ft_orders, COALESCE(SUM(lta_subs),0) as lt_orders
         FROM initial_s3_data
-        GROUP BY 1,2,3,4,5,6,7
+        LEFT JOIN (SELECT utm_campaign::varchar, campaign_name FROM gsheet_raw.utm_campaign_list) USING(utm_campaign)
+        GROUP BY 1,2,3,4,5,6,7,8
         {% if not loop.last %}UNION ALL
         {% endif %}
     {% endfor %}),
   
     final_data as
-    (SELECT channel, date::date, date_granularity, market, product, utm_campaign, campaign_type, utm_content, utm_term,
+    (SELECT channel, date::date, date_granularity, market, product, google_campaign, utm_campaign, campaign_type, utm_content, utm_term,
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(leads),0) as leads, COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM("VS-01 WK"),0) as "VS-01 WK",
         COALESCE(SUM(revenue),0) as revenue, COALESCE(SUM(ft_orders),0) as ft_orders, COALESCE(SUM(lt_orders),0) as lt_orders
     FROM
-        (SELECT 'Meta' as channel, date, date_granularity, null as market, product, campaign_name as utm_campaign, campaign_type_default as campaign_type,
+        (SELECT 'Meta' as channel, date, date_granularity, null as market, product, null as google_campaign, campaign_name as utm_campaign, campaign_type_default as campaign_type,
             adset_name as utm_content, ad_name as utm_term,
             spend, impressions, link_clicks as clicks, add_to_cart, leads, purchases, "VS-01 WK", 0 as revenue, 0 as ft_orders, 0 as lt_orders
         FROM {{ source('reporting','facebook_ad_performance') }}
         UNION ALL
-        SELECT 'Google Ads' as channel, date, date_granularity, country as market, product, campaign_name as utm_campaign, campaign_type_custom as campaign_type, 
+        SELECT 'Google Ads' as channel, date, date_granularity, country as market, product, campaign_name::varchar as google_campaign, utm_campaign, campaign_type_custom as campaign_type, 
             null as utm_content, null as utm_term,
-            spend, impressions, clicks, 0 as add_to_cart, 0 as leads, purchases, 0 as "VS-01 WK", revenue, 0 as ft_orders, 0 as lt_orders
-        FROM {{ source('reporting','googleads_campaign_performance') }}
+            COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks,
+            0 as add_to_cart, 0 as leads, COALESCE(SUM(purchases),0) as purchases, 0 as "VS-01 WK", COALESCE(SUM(revenue),0) as revenue, 0 as ft_orders, 0 as lt_orders
+        FROM {{ source('reporting','googleads_campaign_performance') }} gc
+        LEFT JOIN s3_data utm ON gc.campaign_name = utm.google_campaign AND gc.date = utm.date AND gc.date_granularity = utm.date_granularity
         WHERE campaign_type_custom NOT IN ('Non Brand','Brand')
+        GROUP BY 1,2,3,4,5,6,7,8,9,10
         UNION ALL
-        SELECT 'Google Ads' as channel, date, date_granularity, country as market, product, campaign_name as utm_campaign, campaign_type_custom as campaign_type, 
+        SELECT 'Google Ads' as channel, date, date_granularity, country as market, product, campaign_name::varchar as google_campaign, utm_campaign, campaign_type_custom as campaign_type, 
             null as utm_content, keyword as utm_term,
-            spend, impressions, clicks, 0 as add_to_cart, 0 as leads, purchases, 0 as "VS-01 WK", revenue, 0 as ft_orders, 0 as lt_orders
-        FROM {{ source('reporting','googleads_keyword_performance') }}
+            COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks,
+            0 as add_to_cart, 0 as leads, COALESCE(SUM(purchases),0) as purchases, 0 as "VS-01 WK", COALESCE(SUM(revenue),0) as revenue, 0 as ft_orders, 0 as lt_orders
+        FROM {{ source('reporting','googleads_keyword_performance') }} gck
+        LEFT JOIN s3_data utm ON gck.campaign_name = utm.google_campaign AND gck.date = utm.date AND gck.date_granularity = utm.date_granularity
+        GROUP BY 1,2,3,4,5,6,7,8,9,10
         UNION ALL
-        SELECT channel, date, date_granularity, market, product, utm_campaign, campaign_type, utm_content, 
+        SELECT channel, date, date_granularity, market, product, google_campaign, utm_campaign, campaign_type, utm_content, 
             CASE WHEN channel = 'Google Ads' AND campaign_type NOT IN ('Non Brand','Brand') THEN null ELSE utm_term END as utm_term,
             0 as spend, 0 as impressions, 0 as clicks, 0 as add_to_cart, 0 as leads, 0 as purchases, 0 as "VS-01 WK", 0 as revenue, ft_orders, lt_orders
         FROM s3_data)
-    GROUP BY channel, date, date_granularity, market, product, utm_campaign, campaign_type, utm_content, utm_term)
+    GROUP BY channel, date, date_granularity, market, product, google_campaign, utm_campaign, campaign_type, utm_content, utm_term)
     
 SELECT channel, 
   date, 
   date_granularity, 
   market, 
   product, 
+  google_campaign,
   utm_campaign, 
   campaign_type, 
   utm_content, 
