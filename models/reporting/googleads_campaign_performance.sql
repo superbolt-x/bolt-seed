6c043c5771282d6402e19003e7996323a2d2ba73
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-10-25 14:18:26 -0600

    Update googleads_campaign_performance.sql
    amazon

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -19,12 +19,13 @@ case
     else 'Other'
 end as product,
 case 
-    when campaign_name ~* 'YT' and campaign_name !~* 'amazon' then 'Youtube'
-    when campaign_name ~* 'Shopping' and campaign_name ~* 'Brand' and campaign_name !~* 'amazon' then 'Shopping - Brand'
-    when campaign_name ~* 'Shopping' and campaign_name !~* 'Brand' and campaign_name !~* 'amazon' then 'Shopping - Non Brand'
-    when (campaign_name ~* 'Performance Max' or campaign_name ~* 'PMax') and campaign_name !~* 'amazon' then 'PMax'
-    when campaign_name ~* 'NB' and campaign_name !~* 'amazon' then 'Non Brand'
-    when campaign_name ~* 'Brand' and campaign_name !~* 'amazon' then 'Branded'
+    when campaign_name ~* 'amazon' then 'Amazon'
+    when campaign_name ~* 'YT' then 'Youtube'
+    when campaign_name ~* 'Shopping' and campaign_name ~* 'Brand' then 'Shopping - Brand'
+    when campaign_name ~* 'Shopping' and campaign_name !~* 'Brand' then 'Shopping - Non Brand'
+    when campaign_name ~* 'Performance Max' or campaign_name ~* 'PMax' then 'PMax'
+    when campaign_name ~* 'NB' then 'Non Brand'
+    when campaign_name ~* 'Brand' then 'Branded'
     else 'Other'
 end as campaign_type_custom,
 date,
