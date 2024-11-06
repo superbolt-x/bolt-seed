545db24eb1acaf047a7e7b68fc049581e87486a2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-06 15:11:31 -0600

    Update blended_performance.sql
    channel

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -18,9 +18,10 @@ WITH last_updated_data as
             utm_campaign::varchar, google_campaign, bing_campaign,
             utm_content, utm_term,
             CASE WHEN channel ~* 'meta' THEN 'Meta' 
-                WHEN channel ~* 'google' OR channel ~* 'youtube' THEN 'Google Ads' 
+                WHEN channel ~* 'google' THEN 'Google Ads' 
+                WHEN channel ~* 'youtube' THEN 'Youtube' 
                 WHEN channel ~* 'bing' THEN 'Bing' 
-            END as channel_adj,
+            END as channel,
             CASE WHEN utm_campaign ~* 'INTL' THEN 'INTL' ELSE 'US' END as market,
             CASE WHEN utm_campaign ~* 'DS01' THEN 'DS01'
                 WHEN utm_campaign ~* 'VS01' THEN 'VS01'
@@ -80,9 +81,10 @@ WITH last_updated_data as
         LEFT JOIN (SELECT utm_campaign, bing_campaign, COUNT(*) FROM s3_data GROUP BY 1,2) utm ON bc.campaign_name = utm.bing_campaign 
         GROUP BY 1,2,3,4,5,6,7,8,9,10,11
         UNION ALL
-        SELECT channel_adj as channel, date, date_granularity, market, product, google_campaign, bing_campaign, utm_campaign, campaign_type, 
-            CASE WHEN channel_adj = 'Google Ads' THEN null ELSE utm_content END as utm_content, 
-            CASE WHEN channel_adj = 'Google Ads' THEN null ELSE utm_term END as utm_term,
+        SELECT CASE WHEN channel = 'Google Ads' OR channel = 'Youtube' THEN 'Google Ads' ELSE channel END as channel, date, date_granularity, market, product, 
+            google_campaign, bing_campaign, utm_campaign, campaign_type, 
+            CASE WHEN channel = 'Google Ads' OR channel = 'Bing' THEN null ELSE utm_content END as utm_content, 
+            CASE WHEN channel = 'Google Ads' OR channel = 'Bing' THEN null ELSE utm_term END as utm_term,
             0 as spend, 0 as impressions, 0 as clicks, 0 as checkout_initiated, 0 as add_to_cart, 0 as leads, 0 as purchases, 0 as "VS-01 WK", 0 as revenue, ft_orders, lt_orders
         FROM s3_data)
     GROUP BY channel, date, date_granularity, market, product, google_campaign, bing_campaign, utm_campaign, campaign_type, utm_content, utm_term)
