ce480051c53a9ac33b4a56abb9cfae62280b4bc0
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-06 13:37:33 -0600

    Update bingads_campaign_performance.sql
    country, product, campaign_type_custom

--- models/reporting/bingads_campaign_performance.sql
+++ models/reporting/bingads_campaign_performance.sql
@@ -8,6 +8,24 @@ campaign_name,
 campaign_id,
 campaign_status,
 campaign_type_default,
+case 
+    when campaign_name ~* 'INTL' then 'INTL'
+    else 'US'
+end as country,
+case 
+    when campaign_name ~* 'DS01' then 'DS01'
+    when campaign_name ~* 'VS01' then 'VS01'
+    when campaign_name ~* 'PDS08' then 'PDS08'
+    else 'Other'
+end as product,
+case 
+    when campaign_name ~* 'amazon' then 'Amazon'
+    when campaign_name ~* 'Shopping' and campaign_name ~* 'Brand' then 'Shopping - Brand'
+    when campaign_name ~* 'Shopping' and campaign_name !~* 'Brand' then 'Shopping - Non Brand'
+    when campaign_name ~* 'NB' then 'Non Brand'
+    when campaign_name ~* 'Brand' then 'Branded'
+    else 'Other'
+end as campaign_type_custom,
 date,
 date_granularity,
 spend,
