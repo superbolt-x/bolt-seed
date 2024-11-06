d26bfd8f7c1835c6d3eefbb17a9dcaf6b0053fe3
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-06 14:30:00 -0600

    Update googleads_campaign_performance.sql
    checkout_initiated

--- models/reporting/googleads_campaign_performance.sql
+++ models/reporting/googleads_campaign_performance.sql
@@ -37,5 +37,37 @@ purchaseadwordspixel as purchases,
 purchaseadwordspixel_value as revenue,
 search_impression_share,
 search_budget_lost_impression_share,
-search_rank_lost_impression_share
+search_rank_lost_impression_share,
+checkout_initiated
 FROM {{ ref('googleads_performance_by_campaign') }}
+LEFT JOIN 
+    (SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity,
+        customer_id as account_id, id as campaign_id, name as campaign_name,
+        COALESCE(SUM(CASE WHEN conversion_action_name = 'Checkout Started' THEN conversions END),0) as checkout_initiated
+    FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
+    GROUP BY 1,2,3,4,5
+    UNION ALL
+    SELECT DATE_TRUNC('week',date) as date, 'week' as date_granularity,
+        customer_id as account_id, id as campaign_id, name as campaign_name,
+        COALESCE(SUM(CASE WHEN conversion_action_name = 'Checkout Started' THEN conversions END),0) as checkout_initiated
+    FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
+    GROUP BY 1,2,3,4,5
+    UNION ALL
+    SELECT DATE_TRUNC('month',date) as date, 'month' as date_granularity,
+        customer_id as account_id, id as campaign_id, name as campaign_name,
+        COALESCE(SUM(CASE WHEN conversion_action_name = 'Checkout Started' THEN conversions END),0) as checkout_initiated
+    FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
+    GROUP BY 1,2,3,4,5
+    UNION ALL
+    SELECT DATE_TRUNC('quarter',date) as date, 'quarter' as date_granularity,
+        customer_id as account_id, id as campaign_id, name as campaign_name,
+        COALESCE(SUM(CASE WHEN conversion_action_name = 'Checkout Started' THEN conversions END),0) as checkout_initiated
+    FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
+    GROUP BY 1,2,3,4,5
+    UNION ALL
+    SELECT DATE_TRUNC('year',date) as date, 'year' as date_granularity,
+        customer_id as account_id, id as campaign_id, name as campaign_name,
+        COALESCE(SUM(CASE WHEN conversion_action_name = 'Checkout Started' THEN conversions END),0) as checkout_initiated
+    FROM {{ source('googleads_raw','campaign_convtype_performance_report') }}
+    GROUP BY 1,2,3,4,5
+    ) USING (date, date_granularity, account_id, campaign_id, campaign_name)
