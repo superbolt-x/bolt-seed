95f96779ebd9c7cd2b163b1fcbbb61bdcc9fed6c
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-07 10:40:22 -0600

    Update blended_performance_keyword.sql
    bing

--- models/reporting/blended_performance_keyword.sql
+++ models/reporting/blended_performance_keyword.sql
@@ -15,9 +15,13 @@ WITH last_updated_data as
     s3_data as
     ({%- for date_granularity in date_granularity_list %}    
         SELECT '{{date_granularity}}' as date_granularity, {{date_granularity}} as date,
-            utm_campaign::varchar, campaign_name::varchar as google_campaign,
+            utm_campaign::varchar, google_campaign, bing_campaign,
             utm_content, utm_term,
-            CASE WHEN channel ~* 'meta' THEN 'Meta' WHEN channel ~* 'google' THEN 'Google Ads' END as channel,
+            CASE WHEN channel ~* 'meta' THEN 'Meta' 
+                WHEN channel ~* 'google' THEN 'Google Ads' 
+                WHEN channel ~* 'youtube' THEN 'Youtube' 
+                WHEN channel ~* 'bing' THEN 'Bing' 
+            END as channel,
             CASE WHEN utm_campaign ~* 'INTL' THEN 'INTL' ELSE 'US' END as market,
             CASE WHEN utm_campaign ~* 'DS01' THEN 'DS01'
                 WHEN utm_campaign ~* 'VS01' THEN 'VS01'
@@ -27,7 +31,7 @@ WITH last_updated_data as
             END as product,    
             CASE 
                 WHEN campaign_name ~* 'amazon' THEN 'Amazon'
-                WHEN campaign_name ~* 'YT' THEN 'Youtube'
+                WHEN campaign_name ~* 'YT' OR channel ~* 'youtube' THEN 'Youtube'
                 WHEN campaign_name ~* 'Shopping' AND campaign_name ~* 'Brand' THEN 'Shopping - Brand'
                 WHEN campaign_name ~* 'Shopping' AND campaign_name !~* 'Brand' THEN 'Shopping - Non Brand'
                 WHEN campaign_name ~* 'Performance Max' OR campaign_name ~* 'PMax' THEN 'PMax'
@@ -39,19 +43,22 @@ WITH last_updated_data as
             END as campaign_type,
             COALESCE(SUM(fta_subs),0) as ft_orders, COALESCE(SUM(lta_subs),0) as lt_orders
         FROM initial_s3_data
-        LEFT JOIN (SELECT utm_campaign::varchar, campaign_name, COUNT(*) FROM {{ source('gsheet_raw','utm_campaign_list') }} GROUP BY 1,2) USING(utm_campaign)
-        WHERE channel ~* 'google'
-        GROUP BY 1,2,3,4,5,6,7,8
+        LEFT JOIN (SELECT CASE WHEN channel ~* 'google' THEN campaign_name::varchar END as google_campaign, CASE WHEN channel ~* 'bing' THEN campaign_name::varchar END as bing_campaign, 
+                utm_campaign::varchar, COUNT(*) 
+            FROM {{ source('gsheet_raw','utm_campaign_list') }} 
+            GROUP BY 1,2,3) USING(utm_campaign)
+        WHERE (channel ~* 'google' OR channel ~* 'bing')
+        GROUP BY 1,2,3,4,5,6,7,8,9,10,11
         {% if not loop.last %}UNION ALL
         {% endif %}
     {% endfor %}),
   
     final_data as
-    (SELECT channel, date::date, date_granularity, market, product, google_campaign, utm_campaign, campaign_type, utm_term,
+    (SELECT channel, date::date, date_granularity, market, product, google_campaign, bing_campaign, utm_campaign, campaign_type, utm_term,
         COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue, COALESCE(SUM(ft_orders),0) as ft_orders, COALESCE(SUM(lt_orders),0) as lt_orders
     FROM
-        (SELECT 'Google Ads' as channel, gck.date, gck.date_granularity, country as market, product, campaign_name::varchar as google_campaign, utm_campaign, 
+        (SELECT 'Google Ads' as channel, gck.date, gck.date_granularity, country as market, product, google_campaign, null as bing_campaign, utm_campaign, 
             campaign_type_custom as campaign_type, keyword as utm_term,
             COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks,
             COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue, 0 as ft_orders, 0 as lt_orders
@@ -59,10 +66,18 @@ WITH last_updated_data as
         LEFT JOIN (SELECT utm_campaign, google_campaign, COUNT(*) FROM s3_data GROUP BY 1,2) utm ON gck.campaign_name = utm.google_campaign 
         GROUP BY 1,2,3,4,5,6,7,8,9
         UNION ALL
-        SELECT channel, date, date_granularity, market, product, google_campaign, utm_campaign, campaign_type, utm_term,
+        SELECT 'Bing' as channel, bk.date, bk.date_granularity, country as market, product, null as google_campaign, bing_campaign, utm_campaign, 
+            campaign_type_custom as campaign_type, keyword as utm_term,
+            COALESCE(SUM(spend),0) as spend, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(clicks),0) as clicks,
+            COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue, 0 as ft_orders, 0 as lt_orders
+        FROM {{ source('reporting','bingads_keyword_performance') }} bk
+        LEFT JOIN (SELECT utm_campaign, bing_campaign, COUNT(*) FROM s3_data GROUP BY 1,2) utm ON bk.campaign_name = utm.bing_campaign 
+        GROUP BY 1,2,3,4,5,6,7,8,9
+        UNION ALL
+        SELECT channel, date, date_granularity, market, product, google_campaign::varchar, bing_campaign::varchar, utm_campaign::varchar, campaign_type::varchar, utm_term::varchar,
             0 as spend, 0 as impressions, 0 as clicks, 0 as purchases, 0 as revenue, ft_orders, lt_orders
         FROM s3_data)
-    GROUP BY channel, date, date_granularity, market, product, google_campaign, utm_campaign, campaign_type, utm_term)
+    GROUP BY channel, date, date_granularity, market, product, google_campaign, bing_campaign, utm_campaign, campaign_type, utm_term)
     
 SELECT channel, 
   date, 
@@ -70,6 +85,7 @@ SELECT channel,
   market, 
   product, 
   google_campaign,
+  bing_campaign,
   utm_campaign, 
   campaign_type, 
   utm_term,
