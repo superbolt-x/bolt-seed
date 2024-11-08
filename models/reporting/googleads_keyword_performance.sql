3b20e7048212b9771690a0ff51fdfa64afa92def
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-07 18:26:33 -0600

    Update googleads_keyword_performance.sql
    atc

--- models/reporting/googleads_keyword_performance.sql
+++ models/reporting/googleads_keyword_performance.sql
@@ -7,12 +7,13 @@
 WITH initial_data as 
     (SELECT date::date as date, keyword_text as keyword, keyword_match_type as match_type, ad_group_name, ad_group_id, campaign_name, campaign_id,
         COALESCE(SUM(cost_micros/1000000),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions,
-        COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue
+        COALESCE(SUM(purchases),0) as purchases, COALESCE(SUM(revenue),0) as revenue, COALESCE(SUM(add_to_cart),0) as add_to_cart
     FROM {{ source('googleads_raw', 'keyword_performance_report') }}
     LEFT JOIN 
         (SELECT _fivetran_id,date,keyword_text,keyword_match_type,ad_group_name,ad_group_id,campaign_name,campaign_id,
         CASE WHEN conversion_action_name = 'Purchase (Adwords Pixel)' THEN conversions END as purchases, 
-        CASE WHEN conversion_action_name = 'Purchase (Adwords Pixel)' THEN conversions_value END as revenue
+        CASE WHEN conversion_action_name = 'Purchase (Adwords Pixel)' THEN conversions_value END as revenue,
+        CASE WHEN conversion_action_name = 'Add to cart' THEN conversions END as add_to_cart
         FROM {{ source('googleads_raw', 'keyword_convtype_performance_report') }}) 
     USING (_fivetran_id,date,keyword_text,keyword_match_type,ad_group_name,ad_group_id,campaign_name,campaign_id)
     GROUP BY 1,2,3,4,5,6,7),
@@ -44,7 +45,7 @@ cleaned_data as
         else 'Other'
     end as campaign_type_custom,
     COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(purchases),0) as purchases, 
-    COALESCE(SUM(revenue),0) as revenue
+    COALESCE(SUM(revenue),0) as revenue, COALESCE(SUM(add_to_cart),0) as add_to_cart
     FROM cleaned_data
     GROUP BY 1,2,3,4,5,6,7,8,9,10
     {% if not loop.last %}UNION ALL
