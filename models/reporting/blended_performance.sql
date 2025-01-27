f3aa78c9f8a7d0eeeca9e8a036ef9e05e96d35c8
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-27 12:04:37 -0600

    Update blended_performance.sql
    last_updated_data

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -5,14 +5,14 @@
 {% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
   
 WITH last_updated_data as
-    (SELECT *
+    (SELECT _file, _line, _modified, _fivetran_synced, activation_date, activation_month, campaign, adgroup, keyword, lta_subs, utm_term, utm_campaign, fta_subs, utm_content,
+        CASE WHEN utm_campaign ~* 'ds01-demandgen-video' THEN 'GOOGLE' ELSE channel END as channel
     FROM {{ source('s3_raw','lasttouch_performance') }} 
     WHERE _modified IN (SELECT MAX(_modified) FROM {{ source('s3_raw','lasttouch_performance') }} )
     ),
     
     initial_s3_data as 
-    (SELECT *, CASE WHEN utm_campaign ~* 'ds01-demandgen-video' THEN 'GOOGLE' ELSE channel END as channel,
-        {{ get_date_parts('activation_date') }} FROM last_updated_data WHERE utm_campaign IS NOT NULL),
+    (SELECT *, {{ get_date_parts('activation_date') }} FROM last_updated_data WHERE utm_campaign IS NOT NULL),
   
     s3_data as
     ({%- for date_granularity in date_granularity_list %}    
